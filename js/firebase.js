0f110c4db3e2a0d60a376d3352dc802b78f24cbb
aibocah <aibocah86@gmail.com>
2025-12-30 17:51:30 +0700

    Add Firebase initialization script

---
+++ js/firebase.js
@@ -0,0 +1,16 @@
+<script type="module">
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "PROJECT.firebaseapp.com",
+  projectId: "PROJECT",
+  storageBucket: "PROJECT.appspot.com",
+  messagingSenderId: "ID",
+  appId: "APP_ID"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+</script>
